1ea272b33edc4c80e3530507515e19a4fe5e60aa
syosinni <143303290+syosinni@users.noreply.github.com>
2024-06-18 13:13:08 +0900

    Create ダウンロード.py

---
+++ ダウンロード.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+import youtube_dl
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=CFLOiR2EbKM'])
