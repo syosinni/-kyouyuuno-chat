2603dab27b816269db18dfb6b4675e00922735f0
syosinni <143303290+syosinni@users.noreply.github.com>
2024-10-11 11:08:03 +0900

    Create 0001.js

---
+++ 01/ky/js/0001.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const ball = document.getElementById('ball');
+  const whiteBg = document.getElementById('white-bg');
+  const content = document.getElementById('content');
+
+  // 丸の光が終わったら、背景を白くする
+  ball.addEventListener('animationend', function(e) {
+    if (e.animationName === 'glow') {
+      whiteBg.style.opacity = 1;
+
+      // 1秒後にコンテンツを表示する
+      setTimeout(function() {
+        content.style.display = 'block';
+        whiteBg.style.opacity = 0;  // 白い背景を消す
+      }, 1000);
+    }
+  });
+});
