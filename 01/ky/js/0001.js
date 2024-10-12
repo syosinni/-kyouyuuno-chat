7403b759d59b0064fef0c3575eacfe16304e6e6f
syosinni <143303290+syosinni@users.noreply.github.com>
2024-10-12 09:42:44 +0900

    Update 0001.js

--- 01/ky/js/0001.js
+++ 01/ky/js/0001.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ball = document.getElementById('ball');
   const whiteBg = document.getElementById('white-bg');
-  const content = document.getElementById('content');
 
-  // 丸の光が終わったら、背景を白くする
+  // 丸の光が終わったら、背景を白くしてリダイレクト
   ball.addEventListener('animationend', function(e) {
     if (e.animationName === 'glow') {
       whiteBg.style.opacity = 1;
 
+      // 1秒後に別のURLにリダイレクトする
+      setTimeout(function() {
+        window.location.href = 'https://kyouyuuno-to.f5.si/01/ky/home.jp'; // リダイレクト先のURLを指定
+      }, 1000);
     }
   });
 });
